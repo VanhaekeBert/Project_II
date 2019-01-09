e007695d14174e4164a920883373731807bc638d
unknown <arthon.maertens@student.howest.be>
2019-01-09 13:26:01 +0100

    up-to-date code

---
+++ Heartbeattest/test/test/test/Model/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test.Model
+{
+    public class Account
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+
+    }
+}
