6c81ed3dcf07ac113c318d064490de5c77bf8016
VanhaekeBert <bert.vanhaeke@student.howest.be>
2019-01-10 13:58:12 +0100

    Login Page Added

---
+++ Streetworkout/StreetWorkout/StreetWorkout/Views/IntroPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StreetWorkout
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class IntroPage : ContentPage
+	{
+        public IntroPage()
+        {
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                await InitAsync();
+            });
+        }
+        public async Task InitAsync()
+        {
+            Main_Logo.Source = ImageSource.FromResource("StreetWorkout.Assets.Logo_Main.png");
+        }
+
+        private void Button_Login_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
---
+++ Streetworkout/StreetWorkout/StreetWorkout/Views/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StreetWorkout
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+            InitializeComponent();
+            Task.Run(async () =>
+            {
+                await InitAsync();
+            });
+        }
+        public async Task InitAsync()
+        {
+            Main_Logo.Source = ImageSource.FromResource("StreetWorkout.Assets.Logo_Main.png");
+        }
+    }
+}
