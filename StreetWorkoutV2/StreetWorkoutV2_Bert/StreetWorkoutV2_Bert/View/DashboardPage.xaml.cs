57fbf59c4a707d3efec3aabbac5f93436dfd0e58
Kevin Uyttenhove <kevin.uyttenhove@student.howest.be>
2019-01-12 16:50:40 +0100

    Dashboard

--- StreetWorkoutV2/StreetWorkoutV2_Bert/StreetWorkoutV2_Bert/View/DashboardPage.xaml.cs
+++ StreetWorkoutV2/StreetWorkoutV2_Bert/StreetWorkoutV2_Bert/View/DashboardPage.xaml.cs
@@ -16,6 +16,9 @@ namespace StreetWorkoutV2_Bert.View
 		{
 			InitializeComponent ();
             BackgroundImage.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.BackgroundDashboard.png");
+            ImgCal.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Vuur.png");
+            ImgWater.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Beker.png");
+            ImgStartWorkout.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.StartWorkout.png");
         }
 	}
 }
