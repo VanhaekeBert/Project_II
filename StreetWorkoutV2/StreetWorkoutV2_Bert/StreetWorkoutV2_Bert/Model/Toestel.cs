57550413bdaf10af272345a324b12fb56f3f51a0
Arthon <arthon.maertens@student.howest.be>
2019-01-12 20:15:06 +0100

    ToestelPage Finished

---
+++ StreetWorkoutV2/StreetWorkoutV2_Bert/StreetWorkoutV2_Bert/Model/Toestel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace StreetWorkoutV2_Bert.Model
+{
+    public class Toestel
+    {
+        public string Name { get; set; }
+        public int Aantal_Oefeningen { get; set; }
+        public ImageSource Image { get; set; }
+        public ImageSource Go_To_button { get; set; }
+        public ImageSource Background_Frame { get; set; }
+    }
+}
