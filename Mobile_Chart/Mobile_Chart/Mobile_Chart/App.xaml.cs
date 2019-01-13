0674de419c6e877055d1f3e6642701c87d686b77
Arthon <arthon.maertens@student.howest.be>
2019-01-13 22:38:04 +0100

    LineChart Works

--- Mobile_Chart/Mobile_Chart/Mobile_Chart/App.xaml.cs
+++ Mobile_Chart/Mobile_Chart/Mobile_Chart/App.xaml.cs
@@ -1,10 +1,9 @@
-﻿using StreetWorkoutV2_Bert.View;
-using System;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-namespace StreetWorkoutV2_Bert
+namespace Mobile_Chart
 {
     public partial class App : Application
     {
@@ -12,7 +11,7 @@ namespace StreetWorkoutV2_Bert
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new OefeningPage());
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
