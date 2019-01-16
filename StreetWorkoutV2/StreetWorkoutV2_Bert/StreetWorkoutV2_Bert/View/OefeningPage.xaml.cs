57be9528d3f58cc5e86b3c1b53c2e7d8bd06e244
Vanhaeke Bert <bert.vanhaeke@student.howest.be>
2019-01-16 20:25:18 +0100

    Slideshow added

--- StreetWorkoutV2/StreetWorkoutV2_Bert/StreetWorkoutV2_Bert/View/OefeningPage.xaml.cs
+++ StreetWorkoutV2/StreetWorkoutV2_Bert/StreetWorkoutV2_Bert/View/OefeningPage.xaml.cs
@@ -17,30 +17,38 @@ namespace StreetWorkoutV2_Bert.View
         string AantalKeeper = "";
         private int countdownremaining = 0;
         private bool _isRunning = true;
+        private bool _isSlideshowRunning = false;
         Oefening oefeningKeeper = new Oefening();
 		public OefeningPage (Oefening oefening, string aantal)
 		{
 			InitializeComponent ();
             AantalKeeper = aantal;
-            oefeningKeeper = oefening;
+            oefeningKeeper = oefening;  
+
             BckgrImage.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Background.png");
             OefeningCover.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Cover.png");
             OefeningImage.Source = oefening.AfbeeldingenResource[0];
-            //Back button + heartbeat
-            
+
+            if (oefening.AfbeeldingenResource.Capacity <= 1)
+            {
+                SlideshowToggle_Start.IsVisible = false;
+                SlideshowToggle_Stop.IsVisible = false;
+
+            }
+            else
+            {
+                OefeningImage2.Source = oefening.AfbeeldingenResource[1];
+            }
+
             Heart.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Heart.png");
             backbuttonImage.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Backbutton.png");
 
-
-      
-            RunTimer();
-
-            //Pause_Button.Image = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Background.png");
-            // =========================
+            aantal_keer.Text = AantalKeeper;
             Oefeningnaam.Text = oefening.Oefeningnaam;
             herhalingen.Text = oefening.Herhalingen.ToString() + " Herhalingen";
+            oefening.Beschrijving = oefening.Beschrijving.Replace(". ", ". " + Environment.NewLine);
+
             description.Text = oefening.Beschrijving;
-            aantal_keer.Text = AantalKeeper;
 
             backbutton.GestureRecognizers.Add(new TapGestureRecognizer
             {
@@ -50,6 +58,15 @@ namespace StreetWorkoutV2_Bert.View
                     await Navigation.PopAsync();
                 })
             });
+            // -------------------------------------------------------------------
+            // START OF PLAY PAUSE CODE ------------------------------------------
+            // -------------------------------------------------------------------
+
+            RunTimer();
+
+            Pause_Button.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.PauseButton.png");
+            Play_Button.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.PlayButton.png");
+
             TapGestureRecognizer Pause_Play_Gesture = new TapGestureRecognizer
             {
                 Command = new Command(async () =>
@@ -64,6 +81,7 @@ namespace StreetWorkoutV2_Bert.View
                         Play_Button.IsVisible = false;
                         RunTimer();
 
+
                     }
                     else
                     {
@@ -72,47 +90,63 @@ namespace StreetWorkoutV2_Bert.View
                         Pause_Button.IsEnabled = false;
                         Pause_Button.IsVisible = false;
 
+
                     }
                 })
             };
             Pause_Button.GestureRecognizers.Add(Pause_Play_Gesture);
             Play_Button.GestureRecognizers.Add(Pause_Play_Gesture);
+            // -------------------------------------------------------------------
+            // END OF PLAY PAUSE CODE ------------------------------------------
+            // -------------------------------------------------------------------
+
+
+            // -------------------------------------------------------------------
+            // START OF SLIDESHOW CODE ------------------------------------------
+            // -------------------------------------------------------------------
+            SlideshowToggle_Stop.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Slideshow_Pause.png");
+            SlideshowToggle_Start.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Slideshow_Play.png");
+            TapGestureRecognizer Slideshow_Gesture = new TapGestureRecognizer
+            {
+                Command = new Command(async () =>
+                {
+
+                    _isSlideshowRunning = !_isSlideshowRunning;
+                    if (_isSlideshowRunning)
+                    {
+                        SlideshowToggle_Stop.IsEnabled = true;
+                        SlideshowToggle_Stop.IsVisible = true;
+                        SlideshowToggle_Start.IsEnabled = false;
+                        SlideshowToggle_Start.IsVisible = false;
+                        RunSlideshow();
+
+
+                    }
+                    else
+                    {
+                        SlideshowToggle_Start.IsEnabled = true;
+                        SlideshowToggle_Start.IsVisible = true;
+                        SlideshowToggle_Stop.IsEnabled = false;
+                        SlideshowToggle_Stop.IsVisible = false;
+
+
+                    }
+                })
+            };
+
+            SlideshowToggle_Start.GestureRecognizers.Add(Slideshow_Gesture);
+            SlideshowToggle_Stop.GestureRecognizers.Add(Slideshow_Gesture);
+            // -------------------------------------------------------------------
+            // END OF SLIDESHOW CODE --------------------------------------------
+            // -------------------------------------------------------------------
+
         }
-        //public OefeningPage()
-        //{
-        //    InitializeComponent();
-          
-        //    BckgrImage.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Background.png");
-        //    OefeningCover.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Cover.png");
-          
-        //    //Back button + heartbeat
 
-        //    Heart.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Heart.png");
-        //    backbuttonImage.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Backbutton.png");
-        //    Pause_Button.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.PauseButton.png");
-        //    Play_Button.Source = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.PlayButton.png");
+      
 
-
-
-        //    RunTimer();
-
-        //    //Pause_Button.Image = FileImageSource.FromResource("StreetWorkoutV2_Bert.Asset.Oefening_Background.png");
-        //    // =========================
-         
-        //    aantal_keer.Text = AantalKeeper;
-
-        //    backbutton.GestureRecognizers.Add(new TapGestureRecognizer
-        //    {
-        //        Command = new Command(async () => {
-        //            await backbutton.FadeTo(0.3, 500);
-        //            await backbutton.FadeTo(1, 500);
-        //            await Navigation.PopAsync();
-        //        })
-        //    });
-           
-
-        //}
-        
+        // -------------------------------------------------------------------
+        // START OF PLAY PAUSE TIMER CODE ------------------------------------
+        // -------------------------------------------------------------------
 
         public void RunTimer()
         {
@@ -127,19 +161,39 @@ namespace StreetWorkoutV2_Bert.View
                 return _isRunning;
             });
         }
+        // -------------------------------------------------------------------
+        // END OF PLAY PAUSE TIMER CODE --------------------------------------
+        // -------------------------------------------------------------------
 
-        //private void Button_Pause_Clicked(object sender, EventArgs e)
-        //{
+        // -------------------------------------------------------------------
+        // START OF SLIDESHOW TIMER CODE -------------------------------------
+        // -------------------------------------------------------------------
+        bool slideshowstate = false;
 
-        //    _isRunning = !_isRunning;
-        //    if (_isRunning)
-        //    {
-                
-        //        RunTimer();
-        //    }
-        //}
+        public void RunSlideshow()
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(0.8), () => {
+                slideshowstate = !slideshowstate;
+                Device.BeginInvokeOnMainThread(() => {
+                    if (slideshowstate)
+                    {
+                        OefeningImage2.IsVisible = true;
+                    }
+                    else
+                    {
+                        OefeningImage2.IsVisible = false;
 
-       
+                    }
+
+
+                });
+
+                return _isSlideshowRunning;
+            });
+        }
+        // -------------------------------------------------------------------
+        // END OF SLIDESHOW TIMER CODE ---------------------------------------
+        // -------------------------------------------------------------------
 
         private async void Button_Clicked(object sender, EventArgs e)
         {
@@ -152,6 +206,7 @@ namespace StreetWorkoutV2_Bert.View
                 await Navigation.PushAsync(new Oefening_Complete_Page());
             }
         }
+
 
         //protected override bool OnBackButtonPressed()
         //{
